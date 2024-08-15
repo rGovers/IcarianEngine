36eba490736ae3f63b78f0c046be7a43a4047210
rGovers <govers.river.r@gmail.com>
2024-08-15 11:11:37 +1000

    Changed Engine version
    Fixed issues with compoents not being added correctly

--- IcarianNative/BuildIcarianNative.h
+++ IcarianNative/BuildIcarianNative.h
@@ -87,7 +87,7 @@ static CUBE_CProject BuildIcarianNativeProject(e_TargetPlatform a_targetPlatform
 
     CUBE_CProject_AppendDefines(&project,
         "ICARIANNATIVE_VERSION_MAJOR=2024",
-        "ICARIANNATIVE_VERSION_MINOR=1",
+        "ICARIANNATIVE_VERSION_MINOR=2",
         "ICARIANNATIVE_VERSION_PATCH=0",
         commitDefine.Data,
         "ICARIANNATIVE_VERSION_TAG=DEV",
